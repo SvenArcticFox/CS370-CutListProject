7906d0bf116785ea086b192784c0ffa8cdd0b237
SvenArcticFox <74692162+SvenArcticFox@users.noreply.github.com>
2023-09-05 21:48:30 -0400

    Adds Hello World test program to Architectural Spike

---
+++ ArchitecturalSpike/src/main/java/cs370/cutlistproject/architecturalspike/HelloWorldTest.java
@@ -0,0 +1,30 @@
+package cs370.cutlistproject.architecturalspike;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorldTest extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Label helloLabel = new Label("Hello world!");
+
+        Pane pane = new StackPane();
+        pane.getChildren().add(helloLabel);
+
+        Scene sc = new Scene(pane, 200, 200);
+
+        primaryStage.setScene(sc);
+
+        primaryStage.show();
+    }
+}
