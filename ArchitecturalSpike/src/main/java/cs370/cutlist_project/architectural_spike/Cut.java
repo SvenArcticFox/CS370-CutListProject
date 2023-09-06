363075ae3e5b586928a625cd04a6292966ba0187
SvenArcticFox <74692162+SvenArcticFox@users.noreply.github.com>
2023-09-06 12:15:56 -0400

    Adds Cut and Sheet classes

---
+++ ArchitecturalSpike/src/main/java/cs370/cutlist_project/architectural_spike/Cut.java
@@ -0,0 +1,13 @@
+package cs370.cutlist_project.architectural_spike;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cut {
+    private double length;
+    private double width;
+    private Sheet sheet;
+}
---
+++ ArchitecturalSpike/src/main/java/cs370/cutlist_project/architectural_spike/Sheet.java
@@ -0,0 +1,15 @@
+package cs370.cutlist_project.architectural_spike;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sheet {
+
+    private double width;
+    private double length;
+    private double thickness;
+    private String material;
+}
