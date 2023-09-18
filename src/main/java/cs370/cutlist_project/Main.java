0448e99280b48cb2f360245049ce2d1f3b269b22
Christoph2209 <chrissor1soravilla@gmail.com>
2023-09-18 13:29:29 -0400

    Here we go

--- src/main/java/cs370/cutlist_project/Main.java
+++ src/main/java/cs370/cutlist_project/Main.java
@@ -1,16 +1,36 @@
 package cs370.cutlist_project;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/CutListAPp.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+        EventHandler<ActionEvent> eve = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                String num;
+            }
+        };
+
+
+
+    }
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
 }
