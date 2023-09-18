eccedb505731f0feccf5cae1b357e77dd20f5270
SvenArcticFox <74692162+SvenArcticFox@users.noreply.github.com>
2023-09-18 12:19:42 -0400

    Create new cut, sheet, and grain classes

---
+++ src/main/java/cs370/cutlist_project/Cut.java
@@ -0,0 +1,9 @@
+package cs370.cutlist_project;
+
+public class Cut {
+    private double length;
+    private double width;
+    private Grain grain;
+
+
+}
---
+++ src/main/java/cs370/cutlist_project/Sheet.java
@@ -0,0 +1,7 @@
+package cs370.cutlist_project;
+
+public class Sheet {
+    private double length;
+    private double width;
+    private Grain grain;
+}
