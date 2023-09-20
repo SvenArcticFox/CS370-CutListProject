eae9455efa5d96d43f9ee66feef739b3f2575222
Christoph2209 <93446223+Christoph2209@users.noreply.github.com>
2023-09-20 15:35:09 -0400

    squares in squares!

--- src/main/java/cs370/cutlist_project/Controller.java
+++ src/main/java/cs370/cutlist_project/Controller.java
@@ -2,13 +2,17 @@ package cs370.cutlist_project;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Controller {
+    public Pane rectPane;
     Sheet s = new Sheet();
+    Cut c = new Cut();
     @FXML
     public VBox rectBox;
     Rectangle rec = new Rectangle();
@@ -35,14 +39,17 @@ public class Controller {
 
 
     @FXML
-    private TableView<?> cuttingPatternsTable;
+    TableView<Sheet> cuttingPatternsTable = new TableView<Sheet>();
 
     @FXML
     private TableColumn<?, ?> stockSheetColumn;
 
     @FXML
     private TableColumn<?, ?> partsColumn;
-
+    @FXML
+    public TableColumn<Sheet, Double> lengthColumn;
+    @FXML
+    public TableColumn<Sheet, Double> widthColumn;
     //@FXML
     //private Button addStockSheetButton;
 
@@ -51,6 +58,7 @@ public class Controller {
 
     @FXML
     private Button optimizeButton;
+
 
     /*This will be added back soon
 
@@ -74,6 +82,14 @@ public class Controller {
         s.setWidth(Double.parseDouble(stockSheetWidthField.getText()));
         System.out.println("The length is: " + s.getLength() + ". The width is: " + s.getWidth() + "\n" + "The area is: " + s.getTotalArea());
         createRect(rec, s);
+
+        if(!cutLengthField.getText().isEmpty() || !cutWidthField.getText().isEmpty())
+        {
+            c.setLength(Double.parseDouble(cutLengthField.getText()));
+            c.setWidth(Double.parseDouble(cutWidthField.getText()));
+            Rectangle rec2 = new Rectangle();
+            makeCut(rec2,s,c);
+        }
     }
 
     private void createRect(Rectangle rec, Sheet s)
@@ -85,7 +101,23 @@ public class Controller {
         rec.setHeight(s.getLength());
         rec.setFill(Color.RED);
         rec.setStroke(Color.BLACK);
-        rectBox.getChildren().add(rec);
+        rectPane.getChildren().add(rec);
+    }
+
+    private void makeCut(Rectangle rec2, Sheet s, Cut c)
+    {
+        if(s.getLength() < c.getLength() || s.getWidth() < c.getWidth())
+        {
+            System.out.println("DOES NOT WORK");
+
+        }
+        else{
+            rec2.setWidth(c.getWidth());
+            rec2.setHeight(c.getLength());
+            rec2.setFill(Color.GREEN);
+            rec2.setStroke(Color.BLACK);
+            rectPane.getChildren().add(rec2);
+        }
     }
 
 }
