dfbf118cbdd862a3dd65c5926daa2152e99956eb
Christoph2209 <chrissor1soravilla@gmail.com>
2023-09-22 15:57:23 -0400

    laalaa

--- src/main/java/cs370/cutlist_project/Controller.java
+++ src/main/java/cs370/cutlist_project/Controller.java
@@ -52,8 +52,8 @@ public class Controller implements Initializable {
     public TableColumn<Cut, Double> lengthColumn;
     @FXML
     public TableColumn<Cut, Double> widthColumn;
-    //@FXML
-    //private Button addStockSheetButton;
+    @FXML
+    private Button addStockSheetButton;
 
     //@FXML
     //private Button addPartButton;
@@ -73,48 +73,50 @@ public class Controller implements Initializable {
         void handleAddPart(MouseEvent event) {
             // Implement logic to add part to the table
         }
-        */
-/*
+
+
     As of right now, the idea here to have the button be pressed, and the sheet is displayed at the bottom of the screen
     Right now, they show that the sheet is being made, and in the console telling the length, width and area.S
 */
     @FXML
     void handleOptimize(MouseEvent event) {
-        s.setLength(Double.parseDouble(stockSheetLengthField.getText()));
-        s.setWidth(Double.parseDouble(stockSheetWidthField.getText()));
-        System.out.println("The length is: " + s.getLength() + ". The width is: " + s.getWidth() + "\n" + "The area is: " + s.getTotalArea());
-        createRect(s);
-
         if(!cutLengthField.getText().isEmpty() || !cutWidthField.getText().isEmpty())
         {
             Cut c = new Cut();
             c.setLength(Double.parseDouble(cutLengthField.getText()));
             c.setWidth(Double.parseDouble(cutWidthField.getText()));
             Rectangle rec2 = new Rectangle();
-            cutList.add(c);
             makeCut(rec2,s,c);
         }
     }
+    @FXML
+    void sheetCreator(MouseEvent event){
+        s.setLength(Double.parseDouble(stockSheetLengthField.getText()));
+        s.setWidth(Double.parseDouble(stockSheetWidthField.getText()));
+        System.out.println("The length is: " + s.getLength() + ". The width is: " + s.getWidth() + "\n" + "The area is: " + s.getTotalArea());
+        createRect(s);
 
-    private void createRect(Sheet s)
+    }
+    private void createRect(Sheet si)
     {
-        rec.setWidth(s.getWidth());
-        rec.setHeight(s.getLength());
+        rec.setWidth(si.getWidth());
+        rec.setHeight(si.getLength());
         rec.setFill(Color.RED);
         rec.setStroke(Color.BLACK);
     }
-    private void makeCut(Rectangle rec2, Sheet s, Cut c)
+    private void makeCut(Rectangle rec2, Sheet s1, Cut c1)
     {
-        if(s.getLength() < c.getLength() || s.getWidth() < c.getWidth())
+        if(s1.getLength() < c1.getLength() || s1.getWidth() < c1.getWidth())
         {
             System.out.println("DOES NOT WORK");
 
         }
         else{
-            rec2.setWidth(c.getWidth());
-            rec2.setHeight(c.getLength());
+            rec2.setWidth(c1.getWidth());
+            rec2.setHeight(c1.getLength());
             rec2.setFill(Color.GREEN);
             rec2.setStroke(Color.BLACK);
+            cutList.add(c1);
             rectPane.getChildren().add(rec2);
         }
     }
@@ -123,6 +125,7 @@ public class Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         lengthColumn.setCellValueFactory(new PropertyValueFactory<Cut, Double>("length"));
         widthColumn.setCellValueFactory(new PropertyValueFactory<Cut, Double>("width"));
+
         cuttingPatternsTable.setItems(cutList);
     }
 }
