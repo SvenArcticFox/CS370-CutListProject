055b28364653946c935e771c7357635a741cac1c
Christoph2209 <chrissor1soravilla@gmail.com>
2023-10-11 13:17:39 -0400

    this too

--- Cycle2/src/main/java/cs370/cutlistproject/Cycle2/Sheet.java
+++ Cycle2/src/main/java/cs370/cutlistproject/Cycle2/Sheet.java
@@ -1,4 +1,47 @@
 package cs370.cutlistproject.Cycle2;
 
+import lombok.*;
+
+@NoArgsConstructor
 public class Sheet {
+    @Getter
+    private double length;
+    @Getter
+    private double width;
+    @Setter
+    @Getter
+    private Grain grain;
+    @Getter
+    private double totalArea;
+    @Getter
+    private double leftOverArea;
+    @Setter
+    @Getter
+    private double thickness;
+    @Getter
+    private double usedArea;
+    @Setter
+    @Getter
+    private String notes;
+
+    public Sheet(double length, double width) {
+        this.length = length;
+        this.width = width;
+        this.totalArea = length * width;
+    }
+
+    public void setUsedArea(double usedArea) {
+        this.usedArea = usedArea;
+        this.leftOverArea = this.totalArea - this.usedArea;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+        this.totalArea = this.length * this.width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+        this.totalArea = this.length * this.width;
+    }
 }
