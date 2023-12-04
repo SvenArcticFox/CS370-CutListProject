7fe8c7bcd5e1b05360cfea84c90ac09356b5f5ed
Christoph2209 <chrissor1soravilla@gmail.com>
2023-12-04 12:32:56 -0500

    zamn

--- Cycle3/src/main/java/cs370/cutlist_project/Cycle3/Controller.java
+++ Cycle3/src/main/java/cs370/cutlist_project/Cycle3/Controller.java
@@ -8,6 +8,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
@@ -58,6 +59,11 @@ public class Controller implements Initializable  {
     @FXML
     private TableColumn<Cut, String> labelCol;
 
+    @FXML
+    private TableColumn<Cut, Double> xCol;
+    @FXML
+    private TableColumn<Cut, Double> yCol;
+
     ObservableList<Cut> cutList = FXCollections.observableArrayList();
     //Initializes the table view, allowing for the values of the cuts to be shown.
     @Override
@@ -65,6 +71,9 @@ public class Controller implements Initializable  {
         lengthCol.setCellValueFactory(new PropertyValueFactory<>("length"));
         widthCol.setCellValueFactory(new PropertyValueFactory<>("width"));
         labelCol.setCellValueFactory(new PropertyValueFactory<>("cutPartCode"));
+        xCol.setCellValueFactory(new PropertyValueFactory<>("rec.getX()"));
+        yCol.setCellValueFactory(new PropertyValueFactory<>("rec.getY()"));
+
         cutTable.setItems(cutList);
     }
 
@@ -120,7 +129,7 @@ public class Controller implements Initializable  {
             a.showAndWait();
         }
         else {
-            Cut c = new Cut(x,y,cutInputLabel.getText());
+            Cut c = new Cut(y,x,cutInputLabel.getText());
             c.rec.setFill(Color.CYAN);
             c.rec.setStroke(Color.GRAY);
             cutList.add(c);
@@ -134,7 +143,8 @@ public class Controller implements Initializable  {
         {
             recPane.getChildren().add(cut.rec);
             Label l = new Label(cut.getCutPartCode());
-            l.relocate(cut.rec.getX() + (cut.rec.getWidth()/2) -10, cut.rec.getY()+(cut.rec.getHeight()/2) - 10);
+            l.setAlignment(Pos.CENTER);
+            l.relocate(cut.rec.getX() + (cut.rec.getWidth()/2)-10, cut.rec.getY()+(cut.rec.getHeight()/2)-10);
             recPane.getChildren().add(l);
         }
     }
